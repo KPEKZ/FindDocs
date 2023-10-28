dfda0af27853ce10adda38c13aa29b5858c09cf7
Cachap <maksim.dorofeev.95@mail.ru>
2023-10-28 13:54:16 +0300

    feat: add migration and connect to db

---
+++ Backend/FindDox.Services/Services/Document/DocumentService.cs
@@ -0,0 +1,48 @@
+﻿using FindDox.Abstractions.Services.DataAccess;
+using FindDox.Abstractions.Services.Domain;
+using FindDox.Models.Map;
+
+namespace FindDox.Services.Services.Document;
+
+public class DocumentService : IDocumentService
+{
+	protected readonly IDocumentRepository _documentRepository;
+
+	public DocumentService(IDocumentRepository documentRepository)
+	{
+		_documentRepository = documentRepository;
+	}
+
+	public async Task<Models.Api.Document> Get(Guid id)
+	{
+		var doc = await _documentRepository.Get(id);
+		return doc.ToApi();
+	}
+
+	public async Task<Models.Api.Document> Add(Models.Api.Document document)
+	{
+		var doc = document.ToDbo();
+		doc.Keywords = document.Keywords.Select(x => x.ToDbo(doc.Id)).ToList();
+		doc.Links = document.Links.Select(x => x.ToDbo(doc.Id)).ToList();
+
+		var addedDoc = await _documentRepository.Add(doc);
+		await _documentRepository.Save();
+		return addedDoc.ToApi();
+	}
+
+	public async Task<Models.Api.Document> Update(Models.Api.Document document)
+	{
+		var doc = await _documentRepository.Get(document.Id);
+
+		doc.ToUpdateDbo(document);
+
+		await _documentRepository.Update(doc);
+
+		return doc.ToApi();
+	}
+
+	public async Task Remove(Guid id)
+	{
+		await _documentRepository.Remove(id);
+	}
+}
